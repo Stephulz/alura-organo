324a0762ee4ea63e08164f0ca244728bc20f1c55
Stephulz <stefansanches9@gmail.com>
2023-01-02 23:06:53 -0300

    feat: create TextInput component and add google font to root index.css

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders App component", () => {
   render(<App />);
   const linkElement = screen.getByTestId("banner");
   expect(linkElement).toBeInTheDocument();
+
+  // TODO: remove temp TextInput test after creating other components
+  const inputNome = screen.getByTestId("text-input-Nome");
+  expect(screen.getByTestId("text-input-container-Nome")).toBeInTheDocument();
+  expect(screen.getByTestId("text-input-label-Nome")).toBeInTheDocument();
+  expect(inputNome).toBeInTheDocument();
+  fireEvent.change(inputNome, { target: { value: "test nome" } });
+  expect(inputNome.value).toBe("test nome");
+  screen.queryByPlaceholderText(/Digite seu nome/i);
+
+  const inputCargo = screen.getByTestId("text-input-Cargo");
+  expect(screen.getByTestId("text-input-container-Cargo")).toBeInTheDocument();
+  expect(screen.getByTestId("text-input-label-Cargo")).toBeInTheDocument();
+  expect(inputCargo).toBeInTheDocument();
+  fireEvent.change(inputCargo, { target: { value: "test cargo" } });
+  expect(inputCargo.value).toBe("test cargo");
+  screen.queryByPlaceholderText(/Digite seu cargo/i);
+
+  const inputImagem = screen.getByTestId("text-input-Imagem");
+  expect(screen.getByTestId("text-input-container-Imagem")).toBeInTheDocument();
+  expect(screen.getByTestId("text-input-label-Imagem")).toBeInTheDocument();
+  expect(inputImagem).toBeInTheDocument();
+  fireEvent.change(inputImagem, { target: { value: "test imagem" } });
+  expect(inputImagem.value).toBe("test imagem");
+  screen.queryByPlaceholderText(/Informe o endereço da imagem/i);
 });
---
+++ src/components/TextInput/TextInput.test.js
@@ -0,0 +1,13 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TextInput from "./TextInput";
+
+test("renders TextInput component", () => {
+  render(<TextInput label="Nome" placeholder="Digite seu nome" />);
+  const input = screen.getByTestId("text-input-Nome");
+  expect(screen.getByTestId("text-input-container-Nome")).toBeInTheDocument();
+  expect(screen.getByTestId("text-input-label-Nome")).toBeInTheDocument();
+  expect(input).toBeInTheDocument();
+  fireEvent.change(input, { target: { value: "test" } });
+  expect(input.value).toBe("test");
+  screen.queryByPlaceholderText(/Digite seu nome/i);
+});
